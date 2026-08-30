ad43b9715ef2656413dac15de5fe32fe090bb48f
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-30 16:42:03 +0500

    [CL-041] Keep shared transform type ownership explicit

--- src/features/alphabetize-list/model/types.ts
+++ src/features/alphabetize-list/model/types.ts
@@ -2,8 +2,6 @@ import type { TransformListOptions } from "../../list-transform/model/types";
 
 export type AlphabetizeOrder = "asc" | "desc";
 
-export type { TransformListOptions } from "../../list-transform/model/types";
-
 export type AlphabetizeOptions = TransformListOptions & {
   order: AlphabetizeOrder;
 };
