73603fae66a653c3410bbfe5000df4b67f101294
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-30 16:30:33 +0500

    [CL-041] Reuse shared transform options

--- src/features/alphabetize-list/model/types.ts
+++ src/features/alphabetize-list/model/types.ts
@@ -1,9 +1,8 @@
+import type { TransformListOptions } from "../../list-transform/model/types";
+
 export type AlphabetizeOrder = "asc" | "desc";
 
-export type TransformListOptions = {
-  trimWhitespace: boolean;
-  ignoreEmptyLines: boolean;
-};
+export type { TransformListOptions } from "../../list-transform/model/types";
 
 export type AlphabetizeOptions = TransformListOptions & {
   order: AlphabetizeOrder;
